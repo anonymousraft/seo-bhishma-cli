cd6c5337bea2a55b52bdcee1012c0a9532aacff1
Hitendra Rathore <hitendra1995@gmail.com>
2024-06-16 12:05:44 +0530

    version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='seo-bhishma-cli',
-    version='0.1.0',
+    version='1.0',
     author='Hitendra Rathore',
     author_email='hitendra1995@mail.com',
     description='A CLI tool for SEO tasks',
